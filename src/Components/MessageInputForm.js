dc884a40a6c730ef160a099b707a537a3f430a21
Sui.Young <suisauce.codes@gmail.com>
2020-02-28 00:16:01 -0500

    arranged z-index order

--- src/Components/MessageInputForm.js
+++ src/Components/MessageInputForm.js
@@ -2,16 +2,16 @@
 import React, { Component } from "react";
 
 class MessageInputForm extends Component {
-    render() {
+    render(props) {
         return (
-        <div className="MessageInput">
-            <form action="submit" onSubmit={this.handleFormSubmit}>
+        <div className="messageInput">
+            <form action="submit" onSubmit={props.handleFormSubmit}>
             <label htmlFor="messageForm">Please Add A Toy To the ToyBox!</label>
             <input 
                 type="text" 
                 id="messageForm" 
-                onChange={this.handleChange}
-                value={this.state.userInput} // good for accessibility and screen readers, this will track the changes even if they leave and come back
+                onChange={props.handleChange}
+                value={props.state.userInput} // good for accessibility and screen readers, this will track the changes even if they leave and come back
             />
             <button 
                 type="submit" 
