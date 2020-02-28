94015ad640d44eb0da50b228350e9ff0bd922846
Sui.Young <suisauce.codes@gmail.com>
2020-02-27 23:35:50 -0500

    Updates

---
+++ precache-manifest.5c8e12652aacc7d1a4c87da4207339b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb57b7b776f3af39645bcd2952e83bc",
+    "url": "/suiYoungCohort25Chat/index.html"
+  },
+  {
+    "revision": "af7ade08971e05ecb6e1",
+    "url": "/suiYoungCohort25Chat/static/css/main.865679b6.chunk.css"
+  },
+  {
+    "revision": "42fc07f9f68bf00f52b1",
+    "url": "/suiYoungCohort25Chat/static/js/2.56e4c98b.chunk.js"
+  },
+  {
+    "revision": "80f88c8a2fc44f425b1cf753399d693e",
+    "url": "/suiYoungCohort25Chat/static/js/2.56e4c98b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af7ade08971e05ecb6e1",
+    "url": "/suiYoungCohort25Chat/static/js/main.b4ac710c.chunk.js"
+  },
+  {
+    "revision": "0c0d5b2e58111d242547",
+    "url": "/suiYoungCohort25Chat/static/js/runtime-main.c8c72320.js"
+  },
+  {
+    "revision": "f5fca77fdde40680138de763c7605652",
+    "url": "/suiYoungCohort25Chat/static/media/boris-misevic-cQltbypBFvg-unsplash.f5fca77f.jpg"
+  }
+]);
