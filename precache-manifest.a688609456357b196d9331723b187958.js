a80a318fe46fec18b385a8abdc58c6ef4651dac0
Sui.Young <suisauce.codes@gmail.com>
2020-03-03 14:58:39 -0500

    Updates

--- precache-manifest.a688609456357b196d9331723b187958.js
+++ precache-manifest.a688609456357b196d9331723b187958.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2ef31452eafb90b9413d43b51ea3732d",
+    "revision": "0cd8fb196d0b6a9fdf006ace0f5b64b0",
     "url": "/suiYoungCohort25Chat/index.html"
   },
   {
-    "revision": "12dd0bb5473f11fcae60",
-    "url": "/suiYoungCohort25Chat/static/css/main.786469f1.chunk.css"
+    "revision": "af74c28683f96449caf1",
+    "url": "/suiYoungCohort25Chat/static/css/main.f9c5ca87.chunk.css"
   },
   {
     "revision": "b875184b1a11ded36edf",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/suiYoungCohort25Chat/static/js/2.b7519d2a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "12dd0bb5473f11fcae60",
-    "url": "/suiYoungCohort25Chat/static/js/main.7e018b54.chunk.js"
+    "revision": "af74c28683f96449caf1",
+    "url": "/suiYoungCohort25Chat/static/js/main.6d0892b5.chunk.js"
   },
   {
     "revision": "0c0d5b2e58111d242547",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/suiYoungCohort25Chat/precache-manifest.3ce5cb2b46f30766bd4281c304fa0864.js"
+  "/suiYoungCohort25Chat/precache-manifest.a688609456357b196d9331723b187958.js"
 );
 
 self.addEventListener('message', (event) => {
